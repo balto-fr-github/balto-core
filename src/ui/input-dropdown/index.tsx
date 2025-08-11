2ebc195f49301ec876659bb70cbf38c5fc3f8fb1
Eric Prima Wijaya <ericpeweh@gmail.com>
2025-08-11 15:06:26 +0700

    feat: input dropdown component

---
+++ src/ui/input-dropdown/index.tsx
@@ -0,0 +1,12 @@
+export {
+  InputDropdown,
+  type InputDropdownProps,
+  type InputDropdownOption,
+} from "./InputDropdown";
+export {
+  inputDropdownContainerStyles,
+  inputDropdownLabelStyles,
+  inputDropdownWrapperStyles,
+  inputDropdownToggleStyles,
+  inputDropdownFieldStyles,
+} from "./styles";
